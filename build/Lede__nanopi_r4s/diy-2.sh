911427160cde0eb313e9e506c35a613d6df481bf
281677160 <66651997+281677160@users.noreply.github.com>
2021-01-13 12:46:18 +0800

    Update diy-2.sh

--- build/Lede__nanopi_r4s/diy-2.sh
+++ build/Lede__nanopi_r4s/diy-2.sh
@@ -18,13 +18,6 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 
-sed -i '/STAMP_BUILT/d' feeds/packages/utils/runc/Makefile
-sed -i '/STAMP_BUILT/d' feeds/packages/utils/containerd/Makefile
-sed -i "/redirect_https/d" package/network/services/uhttpd/files/uhttpd.config
-sed -i '/Load Average/i\\t\t<tr><td width="33%"><%:CPU Temperature%></td><td><%=luci.sys.exec("cut -c1-2 /sys/class/thermal/thermal_zone0/temp")%><span>&#8451;</span></td></tr>' ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
-sed -i 's/pcdata(boardinfo.system or "?")/"ARMv8"/' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
-
-
 # 修改内核版本
 #sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' target/linux/x86/Makefile
 #sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' target/linux/x86/Makefile
